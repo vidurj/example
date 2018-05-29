5b8e26888b752f1d890a54ed02b78a27b1dc5b3c
vidurj <vidur94@gmail.com>
2018-05-29 14:59:03 -0700

    added requirements

--- setup.py
+++ setup.py
@@ -3,5 +3,6 @@ from setuptools import setup
 setup(
     name='my-awesome-helloworld-script',    # This is the name of your PyPI-package.
     version='0.1',                          # Update the version number for new releases
+    install_requires=['gensim==3.0.0'],     # Requirements
     scripts=['helloworld']                  # The name of your scipt, and also the command you'll be using for calling it
     )
